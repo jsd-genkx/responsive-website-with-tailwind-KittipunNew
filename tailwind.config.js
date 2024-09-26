b534400bad0eeb985278c44f9b208dd133e664fa
Kittipun2710 <new565677@gmail.com>
2024-09-26 15:49:24 +0700

    edit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Anton':["Anton SC", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
